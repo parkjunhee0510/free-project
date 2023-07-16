5b9a2e76f71c92b640fd76a8c90f2a991b293cc5
junhee5143 <junhee5143@naver.com>
2023-07-17 04:23:13 +0900

    0717 park

---
+++ js/1.js
@@ -0,0 +1,9 @@
+function box(com){
+    onclick=document.getElementById('alert').style.display = com;
+
+}
+
+function box2(cum,com){
+    onclick=document.getElementById('alert').style.display = cum;
+    onclick=document.getElementById('alert').innerHTML= com;  
+}
---
+++ mysql.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "< MySQL username >",
+  password: "< MySQL password >",
+  database: "my_db",
+});
+
+connection.connect();
+
+connection.query("SELECT * from Users", (error, rows, fields) => {
+  if (error) throw error;
+  console.log("User info is: ", rows);
+});
+
+connection.end();
